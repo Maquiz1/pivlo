6df0ed8fc446bb0e7810a010a0505500084595e1
Maquiz1 <manquiz92@gmail.com>
2024-02-19 21:34:52 +0300

    fix all54

--- myjs/add/kap/vitu_hatarishi.js
+++ myjs/add/kap/vitu_hatarishi.js
@@ -1,10 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const vitu_hatarishi = document.getElementById("vitu_hatarishi");
-  const vitu_hatarishi_other = document.getElementById("vitu_hatarishi_other");
+const vitu_hatarishi = document.getElementById("vitu_hatarishi");
+const vitu_hatarishi_other = document.getElementById("vitu_hatarishi_other");
 
-  if (vitu_hatarishi.checked && vitu_hatarishi.value === "96") {
+vitu_hatarishi.addEventListener("change", function () {
+  if (this.checked) {
     vitu_hatarishi_other.style.display = "block";
   } else {
     vitu_hatarishi_other.style.display = "none";
   }
 });
+
+// Initial check
+if (vitu_hatarishi.checked) {
+    vitu_hatarishi_other.style.display = "block";
+} else {
+      vitu_hatarishi_other.style.display = "none";
+}
