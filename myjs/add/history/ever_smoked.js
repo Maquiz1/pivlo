0d7650bc193337c165429f81c1aa5322caac1bd9
Maquiz1 <manquiz92@gmail.com>
2024-02-21 17:55:26 +0300

    fix all93

--- myjs/add/history/ever_smoked.js
+++ myjs/add/history/ever_smoked.js
@@ -1,12 +1,18 @@
 const ever_smoked1 = document.getElementById("ever_smoked1");
 const ever_smoked2 = document.getElementById("ever_smoked2");
 
+const start_smoking = document.getElementById("start_smoking");
+const currently_smoking = document.getElementById("currently_smoking");
 const ever_smoked = document.getElementById("ever_smoked");
 
 function toggleElementVisibility() {
   if (ever_smoked1.checked) {
+    start_smoking.style.display = "block";
+    currently_smoking.style.display = "block";
     ever_smoked.style.display = "block";
   } else {
+    start_smoking.style.display = "none";
+    currently_smoking.style.display = "none";
     ever_smoked.style.display = "none";
   }
 }
