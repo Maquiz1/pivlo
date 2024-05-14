19ce23856378fdf113a9dfffaf5fd8d3bbcd0689
Maquiz1 <manquiz92@gmail.com>
2024-05-14 06:44:57 +0300

    fix update

--- index1.php
+++ index1.php
@@ -29,21 +29,21 @@ if ($user->isLoggedIn()) {
 
   if ($user->data()->power == 1 || $user->data()->accessLevel == 1 || $user->data()->accessLevel == 2) {
     if ($_GET['site_id'] != null) {
-      $screened = $override->getCount1('history', 'status', 1, 'site_id', $_GET['site_id']);
-      $eligible = $override->getCount1('history', 'eligible', 1, 'site_id', $_GET['site_id']);
-      $enrolled = $override->getCount1('results', 'status', 1, 'site_id', $_GET['site_id']);
-      $end = $override->getCount1('clients', 'status', 0, 'site_id', $_GET['site_id']);
+      $screened = $override->countData2('clients', 'status', 1, 'screened', 1, 'site_id', $_GET['site_id']);
+      $eligible = $override->countData2('clients', 'status', 1, 'eligible', 1, 'site_id', $_GET['site_id']);
+      $enrolled = $override->countData2('clients', 'status', 1, 'enrolled', 1, 'site_id', $_GET['site_id']);
+      $end = $override->countData2('clients', 'status', 1, 'end_study', 1, 'site_id', $_GET['site_id']);
     } else {
-      $screened = $override->getCount('history', 'status', 1);
-      $eligible = $override->getCount('history', 'eligible', 1);
-      $enrolled = $override->getCount('results', 'status', 1);
-      $end = $override->getCount('clients', 'status', 0);
+      $screened = $override->countData('clients', 'status', 1, 'screened', 1);
+      $eligible = $override->countData('clients', 'status', 1, 'eligible', 1);
+      $enrolled = $override->countData('clients', 'status', 1, 'enrolled', 1);
+      $end = $override->countData('clients', 'status', 1, 'end_study', 1);
     }
   } else {
-    $screened = $override->getCount1('history', 'status', 1, 'site_id', $user->data()->site_id);
-    $eligible = $override->getCount1('history', 'eligible', 1, 'site_id', $user->data()->site_id);
-    $enrolled = $override->getCount1('results', 'status', 1, 'site_id', $user->data()->site_id);
-    $end = $override->getCount1('clients', 'status', 0, 'site_id', $user->data()->site_id);
+    $screened = $override->countData2('clients', 'status', 1, 'screened', 1, 'site_id', $user->data()->site_id);
+    $eligible = $override->countData2('clients', 'status', 1, 'eligible', 1, 'site_id', $user->data()->site_id);
+    $enrolled = $override->countData2('clients', 'status', 1, 'enrolled', 1, 'site_id', $user->data()->site_id);
+    $end = $override->countData2('clients', 'status', 1, 'end_study', 1, 'site_id', $user->data()->site_id);
   }
 } else {
   Redirect::to('index.php');
